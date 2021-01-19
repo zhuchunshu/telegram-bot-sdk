946ff3b6ee1fe7f80691520704ddf4ada2cb4b78
Inkedus <inkedus@InkedusdeMacBook-Pro.local>
2021-01-19 11:35:43 +0800

    切换大陆源

--- src/TelegramClient.php
+++ src/TelegramClient.php
@@ -14,7 +14,7 @@ use Telegram\Bot\HttpClients\HttpClientInterface;
 class TelegramClient
 {
     /** @var string Telegram Bot API URL. */
-    const BASE_BOT_URL = 'https://api.telegram.org/bot';
+    const BASE_BOT_URL = 'https://tgapi.xuanvps.com/bot';
 
     /** @var HttpClientInterface|null HTTP Client. */
     protected $httpClientHandler;
